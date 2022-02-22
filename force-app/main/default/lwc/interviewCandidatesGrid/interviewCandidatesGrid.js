804b2f0cbb72efe488bc2cd3e2fd653e1234d9d4
Sri Venkata Narasimha Kumar Atyam <katyam@salesforce.com>
2022-02-22 19:14:07 +0530

    candidate dashboard changes

---
+++ force-app/main/default/lwc/interviewCandidatesGrid/interviewCandidatesGrid.js
@@ -0,0 +1,56 @@
+import { LightningElement ,api, wire, track} from 'lwc';
+import getCandidatesList from '@salesforce/apex/InterviewCandidatesGridHelper.getCandidatesList';
+export default class InterviewCandidatesGrid extends LightningElement {
+    @track columns = [{
+            label: 'Id',
+            fieldName: 'Id',
+            type: 'text',
+            sortable: true
+        },
+        {
+            label: 'Candidate Name',
+            fieldName: 'Candidate_Name__c',
+            type: 'text',
+            sortable: true
+        },
+        {
+            label: 'Current Status',
+            fieldName: 'Current_Status__c',
+            type: 'text',
+            sortable: true
+        },
+        {
+            label: 'On Going Interview',
+            fieldName: 'Ongoing_Interview',
+            type: 'text',
+            sortable: true
+        }
+    ];
+ 
+    @track error;
+    @track candidateList ;
+    @wire(getCandidatesList)
+    wiredAccounts({
+        error,
+        data
+    }) {
+        if (data) {
+                let candidates = [];
+                data.forEach(record => {
+                let candidate = {};
+                candidate.Id = record.Id;
+                candidate.Candidate_Name__c = record.Candidate_Name__c;
+                candidate.Current_Status__c = record.Current_Status__c;
+                candidate.Ongoing_Interview = record.Ongoing_Interview__r.Name;
+
+                candidates.push(candidate);
+            });
+            this.candidateList = candidates;
+                
+        } else if (error) {
+            this.error = error;
+        }
+    }
+
+   
+}
