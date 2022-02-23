1176ff43a957ca3394f1ebbe23a7da1fad2cc4bc
Vipul Jain <vipul.jain@salesforce.com>
2022-02-23 18:55:36 +0530

    Schedule and Reschedule Action

--- force-app/main/default/lwc/interviewCandidatesGrid/interviewCandidatesGrid.js
+++ force-app/main/default/lwc/interviewCandidatesGrid/interviewCandidatesGrid.js
@@ -1,8 +1,18 @@
 import { LightningElement ,api, wire, track} from 'lwc';
 import getCandidatesList from '@salesforce/apex/InterviewCandidatesGridHelper.getCandidatesList';
-//import getAllAvailableInterviewers from '@salesforce/apex/ScheduleAnInterview.getAllAvailableInterviewers';
+import getAllAvailableInterviewers from '@salesforce/apex/ScheduleAnInterview.getAllAvailableInterviewers';
+import getInterviewRounds from '@salesforce/apex/ScheduleAnInterview.getInterviewRoundsForEventCandidate';
+import scheduleInterview from '@salesforce/apex/ScheduleAnInterview.scheduleInterview';
+import getScheduledRoundForEventCandidate from '@salesforce/apex/ScheduleAnInterview.getScheduledRoundForEventCandidate';
+import getInterviewDetailsByInterviewId from '@salesforce/apex/ScheduleAnInterview.getInterviewDetailsByInterviewId';
 export default class InterviewCandidatesGrid extends LightningElement {
     @api recordId;
+
+    actions = [
+        { label: 'Schedule', name: 'Schedule' },
+        { label: 'Reschedule', name: 'Reschedule'},
+    ];
+
     @track columns = [
         {
             label: 'Id',
@@ -34,7 +44,11 @@ export default class InterviewCandidatesGrid extends LightningElement {
             fieldName: 'Aggregated_Score__c',
             type: 'number',
             sortable: true
-        }
+        },
+        {
+            type: 'action',
+            typeAttributes: { rowActions: this.actions },
+        },
     ];
  
     @track error;
@@ -48,7 +62,7 @@ export default class InterviewCandidatesGrid extends LightningElement {
                 let candidates = [];
                 data.forEach(record => {
                 let candidate = {};
-                candidate.ICName = '/'+record.Id;
+                candidate.ICName = record.Id;
                 candidate.Candidate_Name__c = record.Candidate_Name__c;
                 candidate.Current_Status__c = record.Current_Status__c;
                 candidate.Aggregated_Score__c = record.Aggregated_Score__c;
@@ -66,66 +80,63 @@ export default class InterviewCandidatesGrid extends LightningElement {
     }
 
     selectedCandidates = [];
+    selectedCandidate;
     startDateTime;
     endDateTime;
-    availableInterviewers;
-    getSelectedRows(event) {
-        const selectedRows = event.detail.selectedRows;
-
-        // Display that fieldName of the selected rows
-        for (let i = 0; i < selectedRows.length; i++) {
-            this.selectedCandidates[i] = selectedRows[i].Id;
-        }
-    }
-
+    availableInterviewers = new Map();
+    options;
+    roundName;
     @track isScheduleCandidatesModalOpen = false;
 
     opneScheduleCandidatesModal() {
         this.isScheduleCandidatesModalOpen = true;
+        this.getInterviewRounds();
     }
     closeScheduleCandidatesModal() {
         this.isScheduleCandidatesModalOpen = false;
         this.selectedCandidates = [];
         this.picklistValue = '';
         this.items = [];
-        this.startDateTime = '';
-        this.endDateTime = '';
+        this.startDateTime = undefined;
+        this.endDateTime = undefined;
+        this.selectedCandidate = undefined;
     }
 
     handleStartDateTime(event) {
-        //let a = event.target.value;
         this.startDateTime = event.target.value;
+        if(this.startDateTime !== undefined && this.endDateTime !== undefined)
         this.getInterviewerList();
     }
 
     handleEndDateTime(event) {
-        //let b = event.target.value;
         this.endDateTime = event.target.value;
+        if(this.startDateTime !== undefined && this.endDateTime !== undefined)
         this.getInterviewerList();
     }
 
-    getInterviewerList() {
+    async getInterviewerList() {
         if(this.startDateTime !== undefined && this.endDateTime !== undefined) {
-           this.availableInterviewers ;//= getAllAvailableInterviewers({availabilityCheckFrom : this.startDateTime, availabilityCheckTo : this.endDateTime, eventId : this.recordId});
+            this.availableInterviewers = await getAllAvailableInterviewers({availabilityCheckFrom : this.startDateTime, availabilityCheckTo : this.endDateTime, eventId : this.recordId});
             this.getValues();
         }
     }
 
     @track items = []; //this will hold key, value pair
     picklistValue = ''; //initialize combo box value
-
+    @track options = [];
     @track chosenValue = '';
 
     getValues() {
         if (this.availableInterviewers) {
             let temp = [];
-            for(let i=0; i<this.availableInterviewers.length; i++)  {
+            let i=0;
+            for (const [key, value] of Object.entries(this.availableInterviewers)) {
                 const option = {
-                    label: this.availableInterviewers[i],
-                    value: this.availableInterviewers[i]
+                    label: value,
+                    value: key
                 };
-                temp[i] = option;                                  
-            }  
+                temp[i++] = option;
+            } 
             this.items = temp;              
         } 
     }
@@ -133,5 +144,102 @@ export default class InterviewCandidatesGrid extends LightningElement {
     handleValueChange(event) {
         this.picklistValue = event.detail.value;
     }
+
+    submitDetails() {
+        scheduleInterview({interviewId : this.roundName, availabilityCheckFrom : this.startDateTime, availabilityCheckTo : this.endDateTime, hiringPanelMembers : this.picklistValue})
+        this.closeScheduleCandidatesModal();
+    }
    
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        this.selectedCandidate = row.ICName;
+        switch (actionName) {
+            case 'Schedule':
+                this.opneScheduleCandidatesModal();
+                break;
+            case 'Reschedule':
+                this.opneReScheduleCandidatesModal();
+            default:
+        }
+    }
+    
+
+    async getInterviewRounds() {
+        let temp = await getInterviewRoundsForEventCandidate({interviewEventCandidate : this.selectedCandidate});
+        let temp1 = [];
+        for(let i=0; i<temp.length; i++)  {
+            const option = {
+                label: temp[i].Questionaire__r.Name,
+                value: temp[i].Id
+            };
+            temp1[i] = option;                                  
+        }  
+        this.options = temp1;     
+    }
+
+    handleRoundName(event) {
+        this.roundName = event.target.value;
+    }
+
+    isReScheduleCandidatesModalOpen;
+    startDateTimeValue;
+    endDateTimeValue;
+    resheduledInterviewId;
+    @track rescheduleRoundOptions = [];
+
+    opneReScheduleCandidatesModal() {
+        this.isReScheduleCandidatesModalOpen = true;
+        this.getScheduledRoundForEventCandidate();
+    }
+
+    async getScheduledRoundForEventCandidate() {
+        let temp = await getScheduledRoundForEventCandidate({interviewEventCandidate : this.selectedCandidate});
+        let temp1 = [];
+        for(let i=0; i<temp.length; i++)  {
+            const option = {
+                label: temp[i].Questionaire__r.Name,
+                value: temp[i].Id
+            };
+            temp1[i] = option;                                  
+        }  
+        this.rescheduleRoundOptions = temp1;
+    }
+
+    async getRoundDetails(event) {
+        let temp = await getInterviewDetailsByInterviewId({interviewId : event.target.value});
+        this.startDateTimeValue = temp.StartDateTime;
+        this.endDateTimeValue = temp.EndDateTime;
+        this.resheduledInterviewId = temp.Id;
+        this.availableInterviewers = await getAllAvailableInterviewers({availabilityCheckFrom : this.startDateTimeValue, availabilityCheckTo : this.endDateTimeValue, eventId : this.recordId});
+        this.getValues();
+    }
+
+    
+    submitRescheduleDetails() {
+        scheduleInterview({interviewId : this.resheduledInterviewId, availabilityCheckFrom : this.startDateTimeValue, availabilityCheckTo : this.endDateTimeValue, hiringPanelMembers : this.picklistValue});
+        this.closeRe
+    }
+
+    handleStartDateTimeValue(event) {
+        this.startDateTimeValue = event.target.value;
+        if(this.startDateTimeValue !== undefined && this.endDateTimeValue !== undefined)
+        this.getInterviewerList();
+    }
+
+    handleEndDateTimeValue(event) {
+        this.endDateTime = event.target.value;
+        if(this.startDateTimeValue !== undefined && this.endDateTimeValue !== undefined)
+        this.getRescheduledInterviewerList();
+    }
+
+    closeReScheduleCandidatesModal() {
+        this.isReScheduleCandidatesModalOpen = false;
+        this.selectedCandidates = [];
+        this.picklistValue = '';
+        this.rescheduleRoundOptions = [];
+        this.startDateTimeValue = undefined;
+        this.endDateTimeValue = undefined;
+        this.selectedCandidate = undefined;
+    }
 }
