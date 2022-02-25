0888bc81ae4b1266b69d67fe4629d90e7b0c9cd2
Sajesh Radhakrishnan <sajesh.radhakrishnan@salesforce.com>
2022-02-25 13:06:41 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,18 @@ fi
 sfdx force:org:create -f config/project-scratch-def.json -a $1 -d 30 -s &&
 sfdx force:source:push 
 
-scratchUser=`sfdx force:org:display| tail -1 | awk -F" " '{print($2)}'`
-USERNAME=`sfdx force:data:record:get -s User -w "Username=${scratchUser}"|grep "Email:" | grep "@salesforce.com" | awk -F " " '{print($2)}' | awk -F"@" '{print($1)}'`
+# scratchUser=`sfdx force:org:display| tail -1 | awk -F" " '{print($2)}'`
+# USERNAME=`sfdx force:data:record:get -s User -w "Username=${scratchUser}"|grep "Email:" | grep "@salesforce.com" | awk -F " " '{print($2)}' | awk -F"@" '{print($1)}'`
 
-sfdx force:user:create username=${USERNAME}@$1 email=${USERNAME}@salesforce.com FirstName=Main LastName=Interviewer Alias=main -s generatepassword=false profileName="Interviewer" -u $1 &&
-sfdx force:user:create username=jsmith@$1 email=me@my.org FirstName=Joel LastName=Smith Alias=jsmith -s generatepassword=false profileName="Platform Interviewer" -u $1 &&
-sfdx force:user:create username=jdoe@$1 email=me@my.org FirstName=John LastName=Doe Alias=jdoe -s generatepassword=false profileName="Platform Interviewer" -u $1 &&
-sfdx force:user:create username=xli@$1 email=me@my.orgm FirstName=Xi LastName=Li Alias=xli -s generatepassword=false profileName="Platform Interviewer" -u $1 &&
+# sfdx force:user:create username=${USERNAME}@$1 email=${USERNAME}@salesforce.com FirstName=Main LastName=Interviewer Alias=main -s generatepassword=false profileName="Interviewer" -u $1 &&
+# sfdx force:user:create username=jsmith@$1 email=me@my.org FirstName=Joel LastName=Smith Alias=jsmith -s generatepassword=false profileName="Platform Interviewer" -u $1 &&
+# sfdx force:user:create username=jdoe@$1 email=me@my.org FirstName=John LastName=Doe Alias=jdoe -s generatepassword=false profileName="Platform Interviewer" -u $1 &&
+# sfdx force:user:create username=xli@$1 email=me@my.orgm FirstName=Xi LastName=Li Alias=xli -s generatepassword=false profileName="Platform Interviewer" -u $1 &&
+
+sfdx force:data:record:update -s User -w "Name='User User'" -v "FirstName='Admin' LastName='Harsh' Alias=hbhag TimeZoneSidKey='Asia/Kolkata' LocaleSidKey=en_US LanguageLocaleKey=en_US EmailEncodingKey='UTF-8'" -u $1 &&
+sfdx force:user:create username=sajesh@$1 email=sajesh.radhakrishnan@salesforce.com FirstName=Sajesh LastName=Radhakrishnan Alias=sajesh -s generatepassword=false profileName="Interviewer" -u $1 &&
+sfdx force:user:create username=kpahlajani@$1 email=kpahlajani@salesforce.com FirstName=Kumar LastName=Pahlajani Alias=kpahl -s generatepassword=false profileName="Platform Interviewer" -u $1 &&
+sfdx force:user:create username=katyam@$1 email=me@my.org FirstName=Kumar LastName=Atyam Alias=katyam -s generatepassword=false profileName="Platform Interviewer" -u $1 &&
+sfdx force:user:create username=vipul@$1 email=me@my.orgm FirstName=Vipul LastName=Jain Alias=vipul -s generatepassword=false profileName="Platform Interviewer" -u $1 &&
 
 sfdx sfdmu:run --sourceusername csvfile --targetusername $1 --path force-app/main/default/staticresources/DemoData --verbose
-
-
-
